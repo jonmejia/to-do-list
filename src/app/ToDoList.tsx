364b4830978a823b759c5d1a3ed60bc9e095c466
Jonathan <jonathan@dataminded.net>
2023-06-16 17:13:05 -0400

    added nextui +todolist component

---
+++ src/app/ToDoList.tsx
@@ -0,0 +1,15 @@
+const ToDoList =()=>{
+    return(
+       <div className="flex justify-center">
+         <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <h1 className="text-center text-6xl">to do list</h1>
+            <div>to do list item</div>
+            <div>to do list item</div>
+            <div>to do list item</div>
+            <div>to do list item</div>
+        
+        </div>
+       </div>
+    )
+}
+export default ToDoList;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import Card from "./Card"
+import ToDoList from "./ToDoList"
 export default function Home() {
   const sampleTest: string ="THE ONE PIECE IS REAL!!!"
   return (
-    <Card data={sampleTest}/>
+    <ToDoList/>
   )
 }
