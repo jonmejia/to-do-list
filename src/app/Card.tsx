c4704042fc91a0cd5ff1cde77239b342e981b4c3
Jonathan <jonathan@dataminded.net>
2023-06-17 12:16:12 -0400

    added buttons and an input to todolist

--- src/app/Card.tsx
+++ src/app/Card.tsx
@@ -1,7 +1,12 @@
+'use client'
 const Card = (props:any) =>{
+    const handleDelete = () =>{
+        console.log("delete card")
+    }
     return(
         <div className="max-w-sm rounded overflow-hidden shadow-lg p-2 m-2">
-            <h2 className="text-center">{props.data}</h2>
+            <span className="text-center">{props.data}</span>
+            <button className="m-2 rounded-full bg-emerald-200" onClick={handleDelete}>delete</button>
         </div>
     )
 }
--- src/app/ToDoList.tsx
+++ src/app/ToDoList.tsx
@@ -1,11 +1,16 @@
 import Card from "./Card";
 
 const ToDoList =(props:any)=>{
+
     return(
        <div className="flex justify-center mt-12">
          <div className="max-w-sm rounded overflow-hidden shadow-lg">
             <h1 className="text-center text-4xl">{props.name}&rsquo;s</h1>
             <h2 className="text-center text-4xl">to do list</h2>
+            <div>
+                <input className="m-2" type="text" name="" id="" />
+                <button>add</button>
+            </div>
             <Card data={props.tasks[0]}/>
             <Card data={props.tasks[1]}/>
             <Card data={props.tasks[2]}/>
