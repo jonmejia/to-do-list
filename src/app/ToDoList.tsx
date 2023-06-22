65bccc186ac1fc165deda3ba31e9b8ec5526b5fd
jonmejia <jonathan@dataminded.net>
2023-06-21 23:19:15 -0400

    removed public files and added a handleclick function

--- src/app/ToDoList.tsx
+++ src/app/ToDoList.tsx
@@ -1,22 +1,27 @@
 import Card from "./Card";
 
-const ToDoList =(props:any)=>{
-
-    return(
-       <div className="flex justify-center mt-12">
-         <div className="max-w-sm rounded overflow-hidden shadow-lg">
-            <h1 className="text-center text-4xl">{props.name}&rsquo;s</h1>
-            <h2 className="text-center text-4xl">to do list</h2>
-            <div>
-                <input className="m-2" type="text" name="" id="" />
-                <button>add</button>
-            </div>
-            <Card data={props.tasks[0]}/>
-            <Card data={props.tasks[1]}/>
-            <Card data={props.tasks[2]}/>
-            <Card data={props.tasks[3]}/>
+const ToDoList = (props: any) => {
+    const handleClick = () =>{
+        console.log("nice")
+    }
+  return (
+    <div className="flex justify-center mt-12">
+      <div className="max-w-sm bg-green-200 rounded overflow-hidden drop-shadow-2xl">
+        <h1 className="text-center text-4xl">{props.name}&rsquo;s</h1>
+        <h2 className="text-center text-4xl">to do list</h2>
+        <div>
+          <input className="mx-2" type="text" name="" id="" />
+          <button className="mr-2 pr-2" onClick={handleClick}>
+            add
+          </button>
         </div>
-       </div>
-    )
-}
+        <Card data={props.tasks[0]} />
+        <Card data={props.tasks[1]} />
+        <Card data={props.tasks[2]} />
+        <Card data={props.tasks[3]} />
+      </div>
+    </div>
+  );
+};
+
 export default ToDoList;
