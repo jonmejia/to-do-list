cbc8833c2a70235b172a1aa58d20487aa7d09957
jonmejia <jonathan@dataminded.net>
2023-06-25 02:53:53 -0400

    input now blank after submit

--- src/app/Form.tsx
+++ src/app/Form.tsx
@@ -1,20 +1,21 @@
 'use client'
 import { useState } from "react";
 const Form = () => {
-    const [Task, setTask] = useState()
+    const [Task, setTask] = useState("")
     const handleInput = (e:any) =>{
         setTask(e.target.value)
     }
     const handleSubmit = (e:any) =>{
         e.preventDefault();
         console.log(Task)
+        setTask("")
     }
     return (
     <div className="bg-white rounded-lg shadow-lg p-6">
         <form onSubmit={handleSubmit}>
         <div className="mb-4">
             <label htmlFor="inputText" className="block text-gray-700 text-sm font-bold mb-2">Task</label>
-            <input type="text" className="border border-gray-300 rounded-lg p-2 w-full" onChange={handleInput}/>
+            <input type="text" className="border border-gray-300 rounded-lg p-2 w-full" value={Task} onChange={handleInput}/>
         </div>
             <button
             type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add</button>
